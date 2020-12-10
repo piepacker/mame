890f4414f9907cf9f1d8737606e1b5605295318d
Jean-André Santoni <jean.andre.santoni@gmail.com>
2020-12-10 16:56:32 +0700

    Fix libretro emscripten build

--- src/emu/machine.cpp
+++ src/emu/machine.cpp
@@ -318,7 +318,7 @@ int running_machine::run(bool quiet)
 
 		export_http_api();
 
-#if defined(__EMSCRIPTEN__)
+#if defined(__EMSCRIPTEN__) && !defined(__LIBRETRO__)
 		// break out to our async javascript loop and halt
 		emscripten_set_running_machine(this);
 #endif
