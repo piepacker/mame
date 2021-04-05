9e84eb77ce2583bb4140c03e1cca3501d25e3ffc
Jean-André Santoni <jean.andre.santoni@gmail.com>
2021-04-05 14:08:31 +0700

    Add gaelco2 support in mame tiny

--- scripts/target/mame/tiny.lua
+++ scripts/target/mame/tiny.lua
@@ -18,6 +18,7 @@
 
 CPUS["Z80"] = true
 CPUS["M680X0"] = true
+CPUS["MCS51"] = true
 
 --------------------------------------------------
 -- Specify all the sound cores necessary for the
@@ -25,10 +26,14 @@ CPUS["M680X0"] = true
 --------------------------------------------------
 
 SOUNDS["YM2610"] = true
+SOUNDS["GAELCO_CG1V"] = true
+SOUNDS["GAELCO_GAE1"] = true
 
 --------------------------------------------------
 -- specify available video cores
 --------------------------------------------------
+
+VIDEOS["BUFSPRITE"] = true
 
 --------------------------------------------------
 -- specify available machine cores
@@ -40,7 +45,7 @@ MACHINES["INPUT_MERGER"] = true
 MACHINES["WATCHDOG"] = true
 MACHINES["Z80DAISY"] = true
 MACHINES["UPD1990A"] = true
-
+MACHINES["EEPROMDEV"] = true
 
 --------------------------------------------------
 -- specify available bus cores
@@ -75,7 +80,7 @@ function createProjects_mame_tiny(_target, _subtarget)
 		GEN_DIR  .. "mame/layout",
 	}
 
-files{
+	files{
         MAME_DIR .. "src/mame/drivers/neogeo.cpp",
         MAME_DIR .. "src/mame/includes/neogeo.h",
         MAME_DIR .. "src/mame/video/neogeo.cpp",
@@ -84,7 +89,55 @@ files{
         MAME_DIR .. "src/mame/video/neogeo_spr.h",
         MAME_DIR .. "src/mame/machine/ng_memcard.cpp",
         MAME_DIR .. "src/mame/machine/ng_memcard.h",
-}
+
+		MAME_DIR .. "src/mame/drivers/atvtrack.cpp",
+		MAME_DIR .. "src/mame/drivers/gaelco.cpp",
+		MAME_DIR .. "src/mame/includes/gaelco.h",
+		MAME_DIR .. "src/mame/video/gaelco.cpp",
+		MAME_DIR .. "src/mame/machine/gaelcrpt.cpp",
+		MAME_DIR .. "src/mame/includes/gaelcrpt.h",
+		MAME_DIR .. "src/mame/drivers/gaelco2.cpp",
+		MAME_DIR .. "src/mame/includes/gaelco2.h",
+		MAME_DIR .. "src/mame/machine/gaelco2.cpp",
+		MAME_DIR .. "src/mame/video/gaelco2.cpp",
+		MAME_DIR .. "src/mame/drivers/gaelco3d.cpp",
+		MAME_DIR .. "src/mame/includes/gaelco3d.h",
+		MAME_DIR .. "src/mame/video/gaelco3d.cpp",
+		MAME_DIR .. "src/mame/machine/gaelco3d.cpp",
+		MAME_DIR .. "src/mame/machine/gaelco3d.h",
+		MAME_DIR .. "src/mame/drivers/gaelcopc.cpp",
+		MAME_DIR .. "src/mame/drivers/goldart.cpp",
+		MAME_DIR .. "src/mame/drivers/glass.cpp",
+		MAME_DIR .. "src/mame/includes/glass.h",
+		MAME_DIR .. "src/mame/video/glass.cpp",
+		MAME_DIR .. "src/mame/drivers/mastboy.cpp",
+		MAME_DIR .. "src/mame/drivers/mastboyo.cpp",
+		MAME_DIR .. "src/mame/drivers/rollext.cpp",
+		MAME_DIR .. "src/mame/drivers/splash.cpp",
+		MAME_DIR .. "src/mame/includes/splash.h",
+		MAME_DIR .. "src/mame/video/splash.cpp",
+		MAME_DIR .. "src/mame/drivers/targeth.cpp",
+		MAME_DIR .. "src/mame/includes/targeth.h",
+		MAME_DIR .. "src/mame/video/targeth.cpp",
+		MAME_DIR .. "src/mame/drivers/thoop2.cpp",
+		MAME_DIR .. "src/mame/includes/thoop2.h",
+		MAME_DIR .. "src/mame/video/thoop2.cpp",
+		MAME_DIR .. "src/mame/drivers/wrally.cpp",
+		MAME_DIR .. "src/mame/includes/wrally.h",
+		MAME_DIR .. "src/mame/machine/wrally.cpp",
+		MAME_DIR .. "src/mame/video/wrally.cpp",
+		MAME_DIR .. "src/mame/drivers/blmbycar.cpp",
+		MAME_DIR .. "src/mame/includes/blmbycar.h",
+		MAME_DIR .. "src/mame/video/blmbycar.cpp",
+		MAME_DIR .. "src/mame/video/gaelco_wrally_sprites.cpp",
+		MAME_DIR .. "src/mame/video/gaelco_wrally_sprites.h",
+		MAME_DIR .. "src/mame/drivers/xorworld.cpp",
+		MAME_DIR .. "src/mame/includes/xorworld.h",
+		MAME_DIR .. "src/mame/video/xorworld.cpp",
+		MAME_DIR .. "src/mame/machine/gaelco_ds5002fp.cpp",
+		MAME_DIR .. "src/mame/machine/gaelco_ds5002fp.h",
+		MAME_DIR .. "src/mame/drivers/bigkarnk_ms.cpp",
+	}
 end
 
 function linkProjects_mame_tiny(_target, _subtarget)
